a9e5f6ddd0ba1338e49fba56d513262584644fb0
Stacia <git.stacia@xoxy.net>
2014-06-12 20:42:13 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RatingsGraph::Application.config.secret_key_base = '917071fd19adbebd0c8a27ed9d167110042f22ef265da3858545a16c99f6fec75d5e3f934be8f637dc3c71feabb1d6e9cd063929adb49d3e261b63e04660bd84'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RatingsGraph::Application.config.session_store :cookie_store, key: '_ratings-graph_session'
